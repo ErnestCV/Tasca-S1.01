b042e1534bbd5babf17d68b4876264e0c69df983
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-06-15 03:26:05 +0200

    Add files via upload

---
+++ n1ex1/src/n1ex1/Instrument.java
@@ -0,0 +1,61 @@
+package n1ex1;
+
+public abstract class Instrument {
+
+    String nom;
+    float preu;
+    public Instrument(String nom, float preu){
+        this.nom = nom;
+        this.preu = preu;
+    }
+
+    static void tocar(){}
+
+    public static void main(String[] args) {
+
+        //Per carregar una classe podem:
+
+        //Crear una instància d'una classe
+        Vent violi = new Vent("Violi", 200);
+        violi.tocar();
+
+        //Accedir a un membre estàtic d'una classe
+        Corda.tocar();
+
+    }
+}
+
+class Vent extends Instrument{
+
+    public Vent(String nom, float preu) {
+        super(nom, preu);
+    }
+
+    static void tocar(){
+        System.out.println("Està sonant un instrument de vent");
+    }
+
+}
+
+class Corda extends Instrument{
+
+    public Corda(String nom, float preu) {
+        super(nom, preu);
+    }
+
+    static void tocar(){
+        System.out.println("Està sonant un instrument de corda");
+    }
+
+}
+
+class Percussio extends Instrument{
+
+    public Percussio(String nom, float preu) {
+        super(nom, preu);
+    }
+
+    static void tocar(){
+        System.out.println("Està sonant un instrument de percussió");
+    }
+}
