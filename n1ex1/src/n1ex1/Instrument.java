d4a7def07cde45c90af5fc19acfa33588f2c790f
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-06-15 16:19:33 +0200

    Test

--- n1ex1/src/n1ex1/Instrument.java
+++ n1ex1/src/n1ex1/Instrument.java
@@ -37,17 +37,7 @@ class Vent extends Instrument{
 
 }
 
-class Corda extends Instrument{
 
-    public Corda(String nom, float preu) {
-        super(nom, preu);
-    }
-
-    static void tocar(){
-        System.out.println("Està sonant un instrument de corda");
-    }
-
-}
 
 class Percussio extends Instrument{
 
