c525083c2b558ef36d69c5e78a7650c14722de50
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-06-15 16:58:31 +0200

    Test

---
+++ src/n1ex2/Main.java
@@ -0,0 +1,14 @@
+package n1ex2;
+
+class Main {
+    public static void main(String[] args) {
+
+        //Invoquem el mètode no estàtic
+        Cotxe cotxe1 = new Cotxe("Astra", 85);
+        cotxe1.accelerar();
+
+        //Invoquem el mètode estàtic (no cal instanciar la classe)
+        Cotxe.frenar();
+
+    }
+}
