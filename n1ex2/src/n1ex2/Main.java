70b37000ee688c6d546c9fdbae1bf3eb0d2731d7
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-06-19 16:24:10 +0200

    Versió revisada

--- n1ex2/src/n1ex2/Main.java
+++ n1ex2/src/n1ex2/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         //Invoquem el mètode no estàtic
-        Cotxe cotxe1 = new Cotxe("Astra", 90);
+        Cotxe cotxe1 = new Cotxe("Astra", 80);
         cotxe1.accelerar();
 
         //Invoquem el mètode estàtic (no cal instanciar la classe)
